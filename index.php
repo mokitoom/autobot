9d1d44cb3fa83cd4fa490740475302e56116c05c
Narongsuk Sihawong <toom_phy@hotmail.com>
2019-01-09 23:50:55 +0700

    Update index.php

--- index.php
+++ index.php
@@ -29,7 +29,7 @@ if (!is_null($events['events']))
 				$respMessage='Hello, your message is '.$event['message']['text'];
 				$httpClient= new CurlHTTPClient($channel_token); 
 				$bot= new LINEBot($httpClient, array('channelSecret'=> $channel_secret)); 
-				$textMessageBuilder= newT extMessageBuilder($respMessage);
+				$textMessageBuilder= new TextMessageBuilder($respMessage);
 				$response=$bot->replyMessage($replyToken, $textMessageBuilder); 
 				break;
 			} 		
