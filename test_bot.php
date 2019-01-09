5ecbd08aaa5f9d5247bb657226ff1b6f8343afb0
Narongsuk Sihawong <toom_phy@hotmail.com>
2019-01-10 00:04:37 +0700

    Update test_bot.php

--- test_bot.php
+++ test_bot.php
@@ -1,5 +1,5 @@
 <?php
-	$access_token = 'XXXXXXXXXXXXXXXXXXXXXXX';
+	$access_token = 'Dmq1hDEyi7LHI8MYzTDC4aljxLt1eu8H6t3bpwgZVZluBhWu+/jsbb6bITP57MYW3+GCcMwvJ2d3Yh5Ck7ifRvw4coVIgwFJRG0H2iRAAU8yU+y4dZVf3lS/drpzTGfOkLUmQAQ50itVR/HuL05DsAdB04t89/1O/w1cDnyilFU=';
 	$url = 'https://api.line.me/v1/oauth/verify';
 	$headers = array('Authorization: Bearer ' . $access_token);$ch = curl_init($url);curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 	curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
