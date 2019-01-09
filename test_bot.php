9ad57a1918ffacb2960c750ce08b82aef308a625
Narongsuk Sihawong <toom_phy@hotmail.com>
2019-01-10 00:54:44 +0700

    Update test_bot.php

--- test_bot.php
+++ test_bot.php
@@ -1,8 +1,9 @@
 <?php
-$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('hb40kGTLBjEgw0J8sDwc2HTTKgv9wXt9NGnxjVf8BXBCgRJUBHp+BU2MOuTzCoPT3+GCcMwvJ2d3Yh5Ck7ifRvw4coVIgwFJRG0H2iRAAU9parABcj3V3yeSsOkxxKP79JWuSexwbml/fZPqOgjPqAdB04t89/1O/w1cDnyilFU=');
-$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => 'b1121d248eb9e38cf2f2f30ebfcf8bc2']);
-
-$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello');
-$response = $bot->replyMessage('<replyToken>', $textMessageBuilder);
-
-echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
+	$access_token = 'hb40kGTLBjEgw0J8sDwc2HTTKgv9wXt9NGnxjVf8BXBCgRJUBHp+BU2MOuTzCoPT3+GCcMwvJ2d3Yh5Ck7ifRvw4coVIgwFJRG0H2iRAAU9parABcj3V3yeSsOkxxKP79JWuSexwbml/fZPqOgjPqAdB04t89/1O/w1cDnyilFU=';
+	$url = 'https://api.line.me/v1/oauth/verify';
+	$headers = array('Authorization: Bearer ' . $access_token);$ch = curl_init($url);curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+	$result = curl_exec($ch);
+	curl_close($ch);
+	echo $result;
