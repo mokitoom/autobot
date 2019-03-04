efb928890316f01316d38927639261311e5cfe94
Narongsuk Sihawong <toom_phy@hotmail.com>
2019-03-04 19:28:18 +0700

    Update index.php

--- index.php
+++ index.php
@@ -4,10 +4,8 @@ require_once('./vendor/autoload.php');
 use \LINE\LINEBot\HTTPClient\CurlHTTPClient;
 use \LINE\LINEBot;
 use \LINE\LINEBot\MessageBuilder\TextMessageBuilder;
-$channel_token = 'pDY5cSpSVUQRUameQ2tqncOfWwQkKfcW9jOeHlPUnF3ZaeOIDxJPOFVtt3TRKYAcvme7AOvdnyxDP8lyfen0gJ4TgWua6QnDL+fHePAhObqHnKQ/KS21HTWEU2Rs8CLOmRr1u52ChdPuGmjZC5m/ygdB04t89/1O/w1cDnyilFU=
-';
+$channel_token = 'pDY5cSpSVUQRUameQ2tqncOfWwQkKfcW9jOeHlPUnF3ZaeOIDxJPOFVtt3TRKYAcvme7AOvdnyxDP8lyfen0gJ4TgWua6QnDL+fHePAhObqHnKQ/KS21HTWEU2Rs8CLOmRr1u52ChdPuGmjZC5m/ygdB04t89/1O/w1cDnyilFU=';
 $channel_secret = '02b4c36856d84cfe9c4e27ca90e8612a';
-
 //Get message from Line API
 $content = file_get_contents('php://input');
 $events=json_decode($content, true);
@@ -17,7 +15,7 @@ if (!is_null($events['events']))
 //Loop through each event 
 	foreach($events['events']as $event)
 	{
-		// Line API send a lot of event type, we interested in message only. 
+// Line API send a lot of event type, we interested in message only. 
 		if ($event['type'] == 'message') 
 		{
 			switch($event['message']['type']) 
@@ -25,18 +23,16 @@ if (!is_null($events['events']))
 				case 'text':
 				//Get replyToken
 				$replyToken = $event['replyToken']; 
-				
 				//Reply message
 				$respMessage='Hello, your message is '.$event['message']['text'];
-				$httpClient= new CurlHTTPClient($channel_token); 
-				$bot= new LINEBot($httpClient, array('channelSecret'=> $channel_secret)); 
-				$textMessageBuilder= new TextMessageBuilder($respMessage);
+				$httpClient=newCurlHTTPClient($channel_token); 
+				$bot=newLINEBot($httpClient, array('channelSecret'=> $channel_secret)); 
+				$textMessageBuilder=newTextMessageBuilder($respMessage);
 				$response=$bot->replyMessage($replyToken, $textMessageBuilder); 
 				break;
-			} 		
+			} 
 		}
 	} 
 }
 
-echo "ok เชื่มต่อได้" ;
-?>
+echo "OK";
