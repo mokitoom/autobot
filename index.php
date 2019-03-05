cc305e2059e3acbbdc72d769130ce56252fcf3b4
Narongsuk Sihawong <toom_phy@hotmail.com>
2019-03-05 17:20:57 +0700

    Update index.php

--- index.php
+++ index.php
@@ -4,8 +4,8 @@ require_once('./vendor/autoload.php');
 use \LINE\LINEBot\HTTPClient\CurlHTTPClient;
 use \LINE\LINEBot;
 use \LINE\LINEBot\MessageBuilder\TextMessageBuilder;
-$channel_token = 'pDY5cSpSVUQRUameQ2tqncOfWwQkKfcW9jOeHlPUnF3ZaeOIDxJPOFVtt3TRKYAcvme7AOvdnyxDP8lyfen0gJ4TgWua6QnDL+fHePAhObqHnKQ/KS21HTWEU2Rs8CLOmRr1u52ChdPuGmjZC5m/ygdB04t89/1O/w1cDnyilFU=';
-$channel_secret = '02b4c36856d84cfe9c4e27ca90e8612a';
+$channel_token = 'yv2hZPSgQSCbw2G3uWoskqx7g/tmphZzkcn5JEMv8WtSQIUKCmp+igFycea76vsVvme7AOvdnyxDP8lyfen0gJ4TgWua6QnDL+fHePAhOboaF0mOo0sNnJFj1QDhWTio9XUJwwixbvYaDmHR4dpIWQdB04t89/1O/w1cDnyilFU=';
+$channel_secret = 'ba09ccef72ff87297da070085814d74a';
 //Get message from Line API
 $content = file_get_contents('php://input');
 $events=json_decode($content, true);
@@ -25,9 +25,9 @@ if (!is_null($events['events']))
 					$replyToken = $event['replyToken']; 
 				//Reply message
 					$respMessage='Hello, your message is '.$event['message']['text'];
-					$httpClient=newCurlHTTPClient($channel_token); 
-					$bot=newLINEBot($httpClient, array('channelSecret'=> $channel_secret)); 
-					$textMessageBuilder=newTextMessageBuilder($respMessage);
+					$httpClient=new CurlHTTPClient($channel_token); 
+					$bot=new LINEBot($httpClient, array('channelSecret'=> $channel_secret)); 
+					$textMessageBuilder=new TextMessageBuilder($respMessage);
 					$response=$bot->replyMessage($replyToken, $textMessageBuilder); 
 				break;
 			} 
@@ -35,4 +35,4 @@ if (!is_null($events['events']))
 	} 
 }
 
-echo "OKxxx";
+echo "conect ok autobot";
